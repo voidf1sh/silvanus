3b490c56756711cf56c0eedcc7a25466b69802a2
Skylar Grant <official.xian@gmail.com>
2023-01-25 03:43:42 -0500

    Fix db connection handling

--- modules/dbfn.js
+++ modules/dbfn.js
@@ -2,13 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 const debugMode = process.env.DEBUG || true;
 const mysql = require('mysql');
-let db = mysql.createConnection({
-	host     : process.env.DBHOST,
-	user     : process.env.DBUSER,
-	password : process.env.DBPASS,
-	database : process.env.DBNAME,
-	port     : process.env.DBPORT
-});
 
 /* Table Structures
 guild_info
@@ -39,26 +32,18 @@ leaderboard
 +-------------+--------------+------+-----+---------+----------------+
 */
 
-db.connect((err) => {
-	if (err) throw `Error connecting to the database: ${err.message}`;
-});
-
-db.on('error', function(err) {
-	db = mysql.createConnection({
-		host     : process.env.DBHOST,
-		user     : process.env.DBUSER,
-		password : process.env.DBPASS,
-		database : process.env.DBNAME,
-		port     : process.env.DBPORT
-	});
-	db.connect((err) => {
-		if (err) throw `Error connecting to the database: ${err.message}`;
-	});
-  });
-  
-
 module.exports = {
 	createGuildTables(guildId) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
         // Create the guild-information and rank-information tables to be used.
 		const createGuildInfoTableQuery = "CREATE TABLE IF NOT EXISTS guild_info(guild_id VARCHAR(50) NOT NULL, tree_name VARCHAR(100) NOT NULL DEFAULT 'Run /setup where your tree is.', tree_height INT(10) NOT NULL DEFAULT 0, tree_message_id VARCHAR(50) NOT NULL DEFAULT 'Run /setup where your tree is.', tree_channel_id VARCHAR(50) NOT NULL DEFAULT 'Run /setup where your tree is.', leaderboard_message_id VARCHAR(50) NOT NULL DEFAULT 'Run /setup where your leaderboard is.', leaderboard_channel_id VARCHAR(50) NOT NULL DEFAULT 'Run /setup where your leaderboard is.', CONSTRAINT guild_pk PRIMARY KEY (guild_id))";
 		const createLeaderboardTableQuery = "CREATE TABLE IF NOT EXISTS leaderboard(id INT(10) NOT NULL AUTO_INCREMENT,guild_id VARCHAR(50) NOT NULL,tree_name VARCHAR(100) NOT NULL,tree_rank INT(10) NOT NULL,tree_height INT(10) NOT NULL DEFAULT 1,has_pin TINYINT(1) NOT NULL DEFAULT 0,timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, CONSTRAINT id_pk PRIMARY KEY(id))";
@@ -68,20 +53,33 @@ module.exports = {
 				if (err) {
 					reject("Error creating the guild_info table: " + err.message);
 					console.error("Offending query: " + createGuildInfoTableQuery);
+					db.end();
 					return;
 				}
 				db.query(createLeaderboardTableQuery, (err) => {
 					if (err) {
 						reject("Error creating the leaderboard table: " + err.message);
 						console.error("Offending query: " + createLeaderboardTableQuery);
+						db.end();
 						return;
 					}
-					resolve({ "status": "Successfully checked both tables.", "data": null })
+					resolve({ "status": "Successfully checked both tables.", "data": null });
+					db.end();
 				});
 			});
 		});
     },
     getGuildInfo(guildId) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
         // Get a server's tree information from the database
 		const selectGuildInfoQuery = `SELECT tree_name, tree_height, tree_message_id, tree_channel_id, leaderboard_message_id, leaderboard_channel_id FROM guild_info WHERE guild_id = ${db.escape(guildId)}`;
 		// TODO run this query and return a promise then structure the output into a GuildInfo object. resolve with { "status": , "data": guildInfo }
@@ -90,6 +88,7 @@ module.exports = {
 				if (err) {
 					console.error(err);
 					reject("Error fetching guild information: " + err.message);
+					db.end();
 					return;
 				}
 				/*const guildInfo = { "guildId": "123",
@@ -102,6 +101,7 @@ module.exports = {
 				};*/
 				if (res.length == 0) {
 					reject("There is no database entry for your guild yet. Try running /setup");
+					db.end();
 					return;
 				}
 				row = res[0];
@@ -113,11 +113,22 @@ module.exports = {
 					"leaderboardMessageId": row.leaderboard_message_id,
 					"leaderboardChannelId": row.leaderboard_channel_id
 				};
-				resolve({ "status": "Successfully fetched guild information", "data": guildInfo })
+				db.end();
+				resolve({ "status": "Successfully fetched guild information", "data": guildInfo });
 			});
 		});
     },
     setGuildInfo(guildInfo) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
 		// Returns a Promise, resolve({ "status": "", "data": null })
 		// guildInfo = { "guildId": "123", "treeName": "name", "treeHeight": 123, "treeMessageId": "123", "treeChannelId": "123", "leaderboardMessageId": "123", "leaderboardChannelId": "123"}
         // Set a server's tree information in the database
@@ -128,13 +139,25 @@ module.exports = {
 				if (err) {
 					console.error(err);
 					reject("Error setting the guild info: " + err.message);
+					db.end();
 					return;
 				}
+				db.end();
 				resolve({ "status": "Successfully set the guild information", "data": null });
 			});
 		});
     },
     setTreeInfo(guildInfo) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
 		// Returns a Promise, resolve({ "status": "", "data": null })
 		// guildInfo = { "guildId": "123", "treeName": "name", "treeHeight": 123, "treeMessageId": "123", "treeChannelId": "123", "leaderboardMessageId": "123", "leaderboardChannelId": "123"}
         // Set a server's tree information in the database)
@@ -144,14 +167,26 @@ module.exports = {
 			db.query(insertGuildInfoQuery, (err, res) => {
 				if (err) {
 					console.error(err);
+					db.end();
 					reject("Error setting the guild info: " + err.message);
 					return;
 				}
+				db.end();
 				resolve({ "status": "Successfully set the guild information", "data": null });
 			});
 		});
     },
     setLeaderboardInfo(guildInfo) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
 		// Returns a Promise, resolve({ "status": "", "data": null })
 		// guildInfo = { "guildId": "123", "treeName": "name", "treeHeight": 123, "treeMessageId": "123", "treeChannelId": "123", "leaderboardMessageId": "123", "leaderboardChannelId": "123"}
         // Set a server's tree information in the database
@@ -161,14 +196,26 @@ module.exports = {
 			db.query(insertGuildInfoQuery, (err, res) => {
 				if (err) {
 					console.error(err);
+					db.end();
 					reject("Error setting the guild info: " + err.message);
 					return;
 				}
+				db.end();
 				resolve({ "status": "Successfully set the guild information", "data": null });
 			});
 		});
     },
 	deleteGuildInfo(guildId) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
 		// Returns a Promise, resolve({ "status": "", "data": null })
 		// guildInfo = { "guildId": "123", "treeName": "name", "treeHeight": 123, "treeMessageId": "123", "treeChannelId": "123", "leaderboardMessageId": "123", "leaderboardChannelId": "123"}
         // Set a server's tree information in the database
@@ -178,14 +225,26 @@ module.exports = {
 			db.query(deleteGuildInfoQuery, (err, res) => {
 				if (err) {
 					console.error(err);
+					db.end();
 					reject("Error deleting the guild info: " + err.message);
 					return;
 				}
+				db.end();
 				resolve({ "status": "Successfully deleted the guild information", "data": null });
 			});
 		});
 	},
     getLeaderboard(guildId) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
 		// Returns a Promise, resolve({ "status": "", "data": leaderboard })
 		const selectLeaderboardQuery = `SELECT id, tree_name, tree_rank, tree_height, has_pin FROM leaderboard WHERE guild_id = ${db.escape(guildId)} ORDER BY id DESC LIMIT 10`;
 		// TODO run the query and return a promise then process the results. resolve with { "status": , "data": leaderboard }
@@ -193,6 +252,7 @@ module.exports = {
 			db.query(selectLeaderboardQuery, (err, res) => {
 				if (err) {
 					console.error(err);
+					db.end();
 					reject("Error fetching the most recent leaderboard: " + err.message);
 					return;
 				}
@@ -205,11 +265,22 @@ module.exports = {
 						"hasPin": row.has_pin
 					});
 				});
+				db.end();
 				resolve({ "status": "Successfully fetched leaderboard.", "data": leaderboard });
 			});
 		});
     },
 	uploadLeaderboard(leaderboard) {
+		const db = mysql.createConnection({
+			host     : process.env.DBHOST,
+			user     : process.env.DBUSER,
+			password : process.env.DBPASS,
+			database : process.env.DBNAME,
+			port     : process.env.DBPORT
+		});
+		db.connect((err) => {
+			if (err) throw `Error connecting to the database: ${err.message}`;
+		});
 		// Returns a Promise, resolve({ "status": "", "data": res })
 		// leaderboard = { "guildId": 1234, "entries": [ { "treeHeight": 12, "treeRank": 34, "treeName": "name", "hasPin": false }, {...}, {...} ] }
 		const insertLeaderboardQuery = "INSERT INTO `leaderboard` (guild_id, tree_name, tree_rank, tree_height, has_pin) VALUES ?";
@@ -221,12 +292,13 @@ module.exports = {
 			db.query(insertLeaderboardQuery, [leaderboardValues], (err, res) => {
 				if (err) {
 					reject("Error uploading the leaderboard: " + err.message);
+					db.end();
 					console.error(err);
 					return;
 				}
+				db.end();
 				resolve({ "status": "Successfully uploaded the leaderboard", "data": res });
 			});
 		});
-	},
-	db
+	}
 };
