d0d40286b14e077052c8066e04215a0f07bdfd90
Skylar Grant <official.xian@gmail.com>
2023-02-09 22:00:12 -0500

    make the beginning height optional, defaulting to the trees current heigt

--- slash-commands/timetoheight.js
+++ slash-commands/timetoheight.js
@@ -8,16 +8,21 @@ module.exports = {
 		.setDescription('Calculate how long it would take to reach a given height')
 		.addStringOption(o => 
 			o.setName('beginheight')
-			 .setDescription('Begining tree height in feet, numbers ONLY')
-			 .setRequired(true))
+			 .setDescription('Begining tree height in feet')
+			 .setRequired(false))
 		.addStringOption(o => 
 			o.setName('endheight')
-			 .setDescription('Ending tree height in feet, numbers ONLY')
+			 .setDescription('Ending tree height in feet')
 			 .setRequired(true)),
 	async execute(interaction) {
 		await interaction.deferReply({ ephemeral: true });
 		const beginHeight = interaction.options.getString('beginheight');
 		const endHeight = interaction.options.getString('endheight');
+		if (!beginHeight) {
+			const getGuildInfoResponse = await dbfn.getGuildInfo(interaction.guildId);
+			const guildInfo = getGuildInfoResponse.data;
+			beginHeight = guildInfo.treeHeight;
+		}
         fn.timeToHeight(beginHeight, endHeight).then(res => {
             interaction.editReply(`It will take a tree that is ${beginHeight}ft tall ${res} to reach ${endHeight}ft.`);
         }).catch(err => {
