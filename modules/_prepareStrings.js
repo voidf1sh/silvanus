a70b27b112e1c1634f3e245fc4547a36f21e7201
Skylar Grant <official.xian@gmail.com>
2023-02-20 15:14:41 -0500

    Documentation update

--- modules/_prepareStrings.js
+++ modules/_prepareStrings.js
@@ -1,34 +1,29 @@
-/*
-</setup:1065407649363005561>
-</setupinfo:1065413032374706196>
-</compare:1065346941166297128>
-</setping:1068373237995683902>
-</optout:1068753032801693758>
-</watertime:1066970330029113444>
-</timetoheight:1067727254634889227>
-</reset:1065412317052944476>
-</help:1065346941166297129>
-</tree:972648557796524032>
-</top trees:1051840665362894950>
-*/
-
+const commands = [
+    {raw: "`/compare`", clickable: "</compare:1077058896469966889>"},
+    {raw: "`/relay set`", clickable: "</relay set:0>"},
+    {raw: "`/relay update`", clickable: "</relay update:0>"},
+    {raw: "`/relay disable`", clickable: "</relay disable:0>"},
+    {raw: "`/rolemenu`", clickable: "</rolemenu:1077058896469966892>"},
+    {raw: "`/watertime`", clickable: "</watertime:1077058896469966895>"},
+    {raw: "`/timetoheight`", clickable: "</timetoheight:1077058896469966894>"},
+    {raw: "`/setup compare`", clickable: "</setup compare:1077058896469966893>"},
+    {raw: "`/setup view`", clickable: "</setup view:1077058896469966893>"},
+    {raw: "`/setup reset`", clickable: "</setup reset:1077058896469966893>"},
+    {raw: "`/help`", clickable: "</help:1077058896469966890>"},
+    {raw: "`/tree`", clickable: "</tree:972648557796524032>"},
+    {raw: "`/top trees`", clickable: "</top trees:1051840665362894950>"},
+    {raw: "`/commands`", clickable: "</commands:1077058896469966888>"}
+];
 const fs = require('fs');
 const replaceAll = require('string.prototype.replaceall');
 const path = "./modules/input.txt";
 const string = fs.readFileSync(path).toString();
 let newString = replaceAll(string, '\* ', '');
-newString = replaceAll(newString, '\r\n', '\\n');
+newString = replaceAll(newString, '\n', '\\n');
 newString = replaceAll(newString, '\t', '  - ');
-newString = replaceAll(newString, '`/setup`', '</setup:1065407649363005561>');
-newString = replaceAll(newString, '`/setupinfo`', '</setupinfo:1065413032374706196>');
-newString = replaceAll(newString, '`/compare`', '</compare:1065346941166297128>');
-newString = replaceAll(newString, '`/watertime`', '</watertime:1066970330029113444>');
-newString = replaceAll(newString, '`/timetoheight`', '</timetoheight:1067727254634889227>');
-newString = replaceAll(newString, '`/reset`', '</reset:1065412317052944476>');
-newString = replaceAll(newString, '`/help`', '</help:1065346941166297129>');
-newString = replaceAll(newString, '`/commands`', '</commands:1069501270454456331>');
-newString = replaceAll(newString, '`/notifications`', '</notifications:0>');
-newString = replaceAll(newString, '`/rolemenu`', '</rolemenu:0>');
+commands.forEach(command => {
+    newString = replaceAll(newString, command.raw, command.clickable);
+});
 newString = replaceAll(newString, '`', '``');
 fs.writeFileSync(path, newString);
 return "Done";
