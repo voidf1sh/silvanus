202a5adcd743ba009f2bbbf2ddbbc23a0d713af2
Skylar Grant <official.xian@gmail.com>
2023-06-05 17:24:05 -0400

    Fix message command

--- dot-commands/message.js
+++ dot-commands/message.js
@@ -3,46 +3,20 @@ const fn = require('../modules/functions.js');
 module.exports = {
 	name: "message",
 	description: "Send a message to a server owner or server",
-	usage: ".message <serverID> <content>",
+	usage: "<serverID> <content>".message,
 	permission: "owner",
 	async execute(message, commandData) {
 		if (fn.dotCommands.checkPermissions(this.permission, message.author.id)) {
 			try {
 				// Code Here
 				args = commandData.args.split(" ");
-				if (message.client.guildInfos.has(args[0])) {
-					let guildInfo = message.client.guildInfos.get(args[0]);
-					const guild = await message.client.guilds.fetch(args[0]).catch(async e => {
-						await message.reply("I was unable to fetch the guild.");
-						console.error(`Error fetching guild to send message: ${e}`);
-					});
-					const guildOwner = await message.client.users.fetch(guild.ownerId).catch(async e => {
-						await message.reply("I was unable to fetch the guild owner.");
-						console.error(`Error fetching guild owner to send message: ${e}`);
-					});
-					await guildOwner.createDM().then(async dm => {
-						await dm.send(args.join(" ")).catch(async e => {
-							await message.reply("I was unable to send the DM.");
-							console.error(`Error sending DM message: ${e}`);
-						});
-					}).catch(async e => {
-						await message.reply("I was unable to create the DM.");
-						console.error(`Error creating DM to send message: ${e}`);
-						const channel = await guild.channels.fetch(guildInfo.reminderChannelId).catch(async e => {
-							await message.reply("I was unable to fetch the channel.");
-							console.error(`Error fetching channel to send message: ${e}`);
-						});
-						await channel.send(args.join(" ")).catch(async e => {
-							await message.reply("I was unable to send the message.");
-							console.error(`Error sending message: ${e}`);
-						});
-					});
-				} else {
-					throw "Guild doesn't exist in database!";
-				}
+				const guildOwnerId = args.shift();
+				const content = args.join(" ");
+				const dmChannel = await message.client.users.createDM(guildOwnerId);
+				await dmChannel.send(content);
 			} catch (err) {
 				console.error(err);
-				await message.reply(fn.builders.errorEmbed("There was an error running the command."));
+				await message.reply(fn.builders.errorEmbed("There was an error running the command: " + err));
 			}
 		}
 	}
