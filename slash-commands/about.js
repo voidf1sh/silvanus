e05bd20f1ffa31f055bf40b9451c949b4271bed5
Skylar Grant <official.xian@gmail.com>
2023-06-07 20:10:49 -0400

    Add about command

---
+++ slash-commands/about.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, messageLink } = require('discord.js');
+const fn = require('../modules/functions.js');
+const strings = require('../data/strings.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('about')
+		.setDescription('Get info about the bot')
+		.addStringOption(o => 
+			o.setName('private')
+			 .setDescription('Should the response be sent privately?')
+			 .setRequired(true)
+			 .addChoices(
+				{ name: "True", value: "true" },
+				{ name: "False", value: "false" })),
+	execute(interaction) {
+		const helpEmbed = fn.builders.helpEmbed(`${strings.help.info}\n\n${strings.help.longDescription}`, interaction.options.getString('private'));
+		interaction.reply(helpEmbed);
+	},
+};
